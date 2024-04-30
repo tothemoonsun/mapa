7b43c0b3ce6457e2f4788c716da6eb1dfa7f9a99
tothemoonsun <matiasleiva.me@gmail.com>
2024-04-30 17:04:49 -0300

    subir todo

--- mapa.py
+++ mapa.py
@@ -1,36 +1,34 @@
+import folium
 import geopandas as gpd
 import pandas as pd
-import folium
 
-# Cargar el archivo Shapefile de provincias
+# Cargar el archivo Shapefile (.shp) de las provincias de Argentina
 provincias = gpd.read_file('provincia.shp')
 
-# Cargar el archivo de datos
+# Cargar los datos del archivo de texto
 data = pd.read_csv('resultados.txt', sep='|', encoding='utf-8')
 
 # Contar el número de ocurrencias de cada provincia
 provincia_counts = data['provincia'].value_counts().reset_index()
 provincia_counts.columns = ['provincia', 'cantidad']
 
-# Unir el DataFrame de conteo con el DataFrame de provincias
-provincias = provincias.merge(provincia_counts, left_on='nam', right_on='provincia', how='left')
+# Unir los datos de conteo con los datos de las provincias
+provincias = provincias.merge(provincia_counts, on='provincia', how='left')
 
-# Crear un mapa interactivo con Folium
+# Crear un mapa centrado en Argentina
 mapa = folium.Map(location=[-38.4161, -63.6167], zoom_start=4)
 
-# Agregar las geometrías de las provincias al mapa
-folium.GeoJson(provincias,
-                name='Provincias',
-                style_function=lambda x: {'fillColor': 'blue', 'fillOpacity': 0.5, 'color': 'black', 'weight': 1},
-                highlight_function=lambda x: {'fillColor': 'red', 'fillOpacity': 0.8},
-                tooltip=folium.features.GeoJsonTooltip(fields=['nam', 'cantidad'],
-                                                        aliases=['Provincia', 'Cantidad'],
-                                                        labels=True,
-                                                        sticky=True)
-                ).add_to(mapa)
+# Agregar las provincias al mapa con círculos proporcionales al número de ocurrencias
+for index, row in provincias.iterrows():
+    folium.Circle(
+        location=[row.geometry.centroid.y, row.geometry.centroid.x],  # Centroides de las provincias
+        radius=row['cantidad'] * 100,  # El radio se multiplica para que los círculos sean más visibles
+        color='blue',
+        fill=True,
+        fill_color='blue',
+        fill_opacity=0.6,
+        tooltip=row['provincia'] + ': ' + str(row['cantidad'])
+    ).add_to(mapa)
 
-# Añadir control de capas
-folium.LayerControl().add_to(mapa)
-
-# Guardar el mapa como archivo HTML
+# Guardar el mapa como un archivo HTML
 mapa.save('mapa_interactivo.html')
